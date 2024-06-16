4d949ace518959cd70c00a67d177d90e2d73c97c
astronaut505 <astronautwave@gmail.com@git commit -m -data>
2024-06-16 02:09:23 +0200

    initial push - works fine

---
+++ build/CMakeFiles/PortfolioPricing.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Me/Documents/GitHub/MonteCarloMischief/src/MonteCarloSimulation.cpp" "CMakeFiles/PortfolioPricing.dir/src/MonteCarloSimulation.cpp.obj" "gcc" "CMakeFiles/PortfolioPricing.dir/src/MonteCarloSimulation.cpp.obj.d"
+  "C:/Users/Me/Documents/GitHub/MonteCarloMischief/src/OptionPricing.cpp" "CMakeFiles/PortfolioPricing.dir/src/OptionPricing.cpp.obj" "gcc" "CMakeFiles/PortfolioPricing.dir/src/OptionPricing.cpp.obj.d"
+  "C:/Users/Me/Documents/GitHub/MonteCarloMischief/src/Portfolio.cpp" "CMakeFiles/PortfolioPricing.dir/src/Portfolio.cpp.obj" "gcc" "CMakeFiles/PortfolioPricing.dir/src/Portfolio.cpp.obj.d"
+  "C:/Users/Me/Documents/GitHub/MonteCarloMischief/src/main.cpp" "CMakeFiles/PortfolioPricing.dir/src/main.cpp.obj" "gcc" "CMakeFiles/PortfolioPricing.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/PortfolioPricing.dir/src/Portfolio.cpp.obj.d
@@ -0,0 +1,4 @@
+CMakeFiles/PortfolioPricing.dir/src/Portfolio.cpp.obj: \
+ C:/Users/Me/Documents/GitHub/MonteCarloMischief/src/Portfolio.cpp \
+ C:/Users/Me/Documents/GitHub/MonteCarloMischief/include/Portfolio.h \
+ C:/Users/Me/Documents/GitHub/MonteCarloMischief/include/OptionPricing.h
